0d2715236d82f5e736ff2d5fbf377b65b2915787
Lauren Avalos <laurenavalos03@gmail.com>
2025-12-17 21:50:23 -0600

    public website

---
+++ marathon/app/Controllers/Home.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('homepage');
+    }
+    public function play($data): string
+    {
+        $data = $data *12;
+        echo('Hello World ->' . $data);
+        exit();
+    }
+}
